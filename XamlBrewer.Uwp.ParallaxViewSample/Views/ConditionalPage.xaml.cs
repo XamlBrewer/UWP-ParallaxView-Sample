e1411572fc06f05e710fd7953849f277f529a56a
Diederik Krols <diederik_krols@hotmail.com>
2018-03-03 08:12:40 +0100

    Declarative and programmatic conditional XAML.

--- XamlBrewer.Uwp.ParallaxViewSample/Views/ConditionalPage.xaml.cs
+++ XamlBrewer.Uwp.ParallaxViewSample/Views/ConditionalPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Windows.Foundation.Metadata;
 using Windows.UI.Xaml.Controls;
 
 namespace XamlBrewer.Uwp.ParallaxViewSample
@@ -8,6 +8,23 @@ namespace XamlBrewer.Uwp.ParallaxViewSample
         public ConditionalPage()
         {
             InitializeComponent();
+
+            Loaded += ConditionalPage_Loaded;
+        }
+
+        private void ConditionalPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 5))
+            {
+                // Insert ParallaxView.
+                var parallax = new ParallaxView();
+                parallax.Source = ForegroundElement;
+                parallax.VerticalShift = 100;
+                Grid.SetColumn(parallax, 2);
+                ContentGrid.Children.Remove(BeachImage);
+                parallax.Child = BeachImage;
+                ContentGrid.Children.Add(parallax);
+            }
         }
     }
 }
